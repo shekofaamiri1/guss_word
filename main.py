e3f16d6a19f55c4853e7087310b58043599713c0
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-23 20:37:00 +0330

    Add else

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ guess_count = len(selected_name)
 guessed_list = ['-']*len(selected_name)
 while guess_count>0:
     gussed_char = input('Enter a char:\n')
-    if gussed_char.isalpha():
+     if gussed_char.isalpha():
         if gussed_char in guessed_list:
             print('you have guessed before,try new chracter')
-        else:
+             else:
+            for idx, char in enumerate(selected_name):
+                if char == gussed_char:
+                    guessed_list[idx]=gussed_char
+            current_guess=' '.join(guessed_list)
+            print(f'perfect=>{current_guess}')
+            
