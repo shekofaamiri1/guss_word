07b026d0d720c3179128bce9c04863723f3c80ac
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-23 20:31:21 +0330

    Add while loop

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ names=['Ali','Amir','sadia','Ahmad']
 selected_name= random.choice(names)
 guess_count = len(selected_name)
 guessed_list = ['-']*len(selected_name)
+while guess_count>0:
+    gussed_char = input('Enter a char:\n')
