a82618274a0be9c6843992e7d31b7204e93c56aa
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-23 20:35:51 +0330

    Add if condition

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ guess_count = len(selected_name)
 guessed_list = ['-']*len(selected_name)
 while guess_count>0:
     gussed_char = input('Enter a char:\n')
+    if gussed_char.isalpha():
+        if gussed_char in guessed_list:
+            print('you have guessed before,try new chracter')
+        else:
